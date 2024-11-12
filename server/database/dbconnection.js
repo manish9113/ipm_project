93b3e688de1b254c89c359490b26d2b682a6b991
mycodesmylife <johndoe@example.com>
2024-11-12 21:06:23 +0530

    commit1

--- server/database/dbconnection.js
+++ server/database/dbconnection.js
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 
 
 
-const Connection=async(USERNAME,PASSWORD)=>{
-    const URL=`mongodb+srv://${USERNAME}:${PASSWORD}@manishcluster.l7yaaki.mongodb.net/?retryWrites=true&w=majority&appName=manishcluster`
-    try{
-        await mongoose.connect(URL)
+const Connection = async (USERNAME, PASSWORD) => {
+    const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@manishcluster.l7yaaki.mongodb.net/?retryWrites=true&w=majority&appName=manishcluster`
+    try {
+        await mongoose.connect(URL, {
+            serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+        })
         console.log("database connected successfully")
-    }catch(error){
-        console.log("error connecting database",error.message)
+    } catch (error) {
+        console.log("error connecting database", error.message)
     }
 }
 
